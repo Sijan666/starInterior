6cc2f790f65253d46e9930862b3ae09fc6fe40a8
Sijan666 <666majharulislam@gmail.com>
2026-05-01 23:46:49 +0600

    add container

--- src/components/common/Container.jsx
+++ src/components/common/Container.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Container() {
+export default function Container({children , className}) {
     return (
         <div className={`${className} w-290 px-3 lg:px-0 mx-auto`}>{children}</div>
     )
