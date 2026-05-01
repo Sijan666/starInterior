f7509226c07e87aa343123ad13c0616724d9b82a
Sijan666 <666majharulislam@gmail.com>
2026-05-02 03:06:39 +0600

    add react icon , fixed button , add nav

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function Button({btnText , className}) {
+export default function Button({ btnText, className, leftIcon, rightIcon }) {
     return (
-        <button className={`${className} customBtn `}><span>{btnText}</span></button>
+        <button className={`${className} customBtn flex items-center gap-2`}>
+            {leftIcon && <span className="icon-left">{leftIcon}</span>}
+            <span>{btnText}</span>
+            {rightIcon && <span className="icon-right">{rightIcon}</span>}
+        </button>
     )
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import Container from '../common/Container'
 import Button from '../common/Button'
+import Image from 'next/image'
+import logo from '../../../public/logo.png'
+import { FaArrowRight } from 'react-icons/fa';
 
 export default function Header() {
     return (
         <>
         <header>
             <Container>
-                <div className="flex mt-5">
-                    <h3>Hello</h3>
-                    <Button btnText={'Contact Us'}/>
+                <div className="py-5 flex justify-between items-center border-b border-white/10">
+                    {/* logo */}
+                    <div className="logo">
+                        <Image src={logo} alt='logo'/>
+                    </div>
+                    {/* navbar */}
+                    <nav>
+                        <ul className='flex items-center gap-x-7'>
+                            <li className='text-white text-base cursor-pointer hover:text-btn duration-300'>Home</li>
+                            <li className='text-white text-base cursor-pointer hover:text-btn duration-300'>Services</li>
+                            <li className='text-white text-base cursor-pointer hover:text-btn duration-300'>Portfolio</li>
+                            <li className='text-white text-base cursor-pointer hover:text-btn duration-300'>Latest Projects</li>
+                            <li className='text-white text-base cursor-pointer hover:text-btn duration-300'>About Us</li>
+                            <li className='text-white text-base cursor-pointer hover:text-btn duration-300'>Blogs</li>
+                        </ul>
+                    </nav>
+                    {/* button */}
+                    <Button btnText={'Contact Us'} rightIcon={<FaArrowRight />} />
                 </div>
             </Container>
         </header>
