1d9d7ac7b9f7fe29b0c6ac628da433c0a9abf18e
Sijan666 <666majharulislam@gmail.com>
2026-05-04 01:18:55 +0600

    feat : add services section and card

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Agency from "@/components/layouts/Agency";
 import Banner from "@/components/layouts/Banner";
 import BestInterior from "@/components/layouts/BestInterior";
 import DreamHome from "@/components/layouts/DreamHome";
+import { Services } from "@/components/layouts/Services";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Services/>
       <Agency />
       <DreamHome />
       <BestInterior/>
