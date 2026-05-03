d4b09a69e98d5fd477c0522d3391587c08a11c44
Sijan666 <666majharulislam@gmail.com>
2026-05-03 23:47:36 +0600

    working on footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Container from '../Container'
+import Image from 'next/image'
+import footerLogo from '../../../public/footerLogo.png'
+
 
 export default function Footer() {
   return (
     <>
     <footer>
-      <h3>This is footer</h3>
+      <div className="pt-17.5 pb-10 bg-[#192324] mt-2">
+        <Container>
+          <div className="flex justify-between items-center border-b border-white/10 pb-7.5">
+            <Image src={footerLogo} alt='footerLogo'/>
+          </div>
+        </Container>
+      </div>
     </footer>
     </>
   )
