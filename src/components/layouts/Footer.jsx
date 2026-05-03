de3feb842a29ab1fcd7839a99b47168502d7bd67
Sijan666 <666majharulislam@gmail.com>
2026-05-03 23:59:19 +0600

    add social icon on footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Container from '../Container'
 import Image from 'next/image'
-import footerLogo from '../../../public/footerLogo.png'
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 
 export default function Footer() {
@@ -11,7 +11,18 @@ export default function Footer() {
       <div className="pt-17.5 pb-10 bg-[#192324] mt-2">
         <Container>
           <div className="flex justify-between items-center border-b border-white/10 pb-7.5">
-            <Image src={footerLogo} alt='footerLogo'/>
+            <Image src="/footerLogo.png" alt='footerLogo' height={86.44} width={182}/>
+            <div className="flex gap-5 items-center">
+              <h3 className='text-[20px] font-bold text-white'>Follow Our Socials</h3>
+              <div className="socialIcons flex gap-2.5 items-center">
+                <FaFacebookF size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaTwitter  size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaInstagram  size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaPinterest  size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaLinkedinIn  size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaYoutube  size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+              </div>
+            </div>
           </div>
         </Container>
       </div>
