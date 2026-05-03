10ad99a6abcf9fe610fc959a755e9f06e90a9dfc
Sijan666 <666majharulislam@gmail.com>
2026-05-03 21:58:04 +0600

    feat : add best interior section - ashfak

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Agency from "@/components/layouts/Agency";
 import Banner from "@/components/layouts/Banner";
+import BestInterior from "@/components/layouts/BestInterior";
 import DreamHome from "@/components/layouts/DreamHome";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Banner />
       <Agency />
       <DreamHome />
+      <BestInterior/>
     </>
   );
 }
