c5fc1a9abfafbadb9a965695c765fb4125ea4896
Sijan666 <666majharulislam@gmail.com>
2026-05-05 23:36:40 +0600

    fix footer icon issue

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -15,19 +15,36 @@ export default function Footer() {
             <div className="flex flex-col md:flex-row gap-5 items-center">
               <h3 className='text-[20px] font-bold text-white'>Follow Our Socials</h3>
               <div className="socialIcons flex gap-2.5 items-center">
-                <FaFacebookF size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
-                <FaTwitter  size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
-                <FaInstagram  size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
-                <FaPinterest  size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
-                <FaLinkedinIn  size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
-                <FaYoutube  size={25} className='p-3 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaFacebookF  className='p-2.5 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaTwitter   className='p-2.5 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaInstagram   className='p-2.5 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaPinterest   className='p-2.5 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaLinkedinIn   className='p-2.5 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
+                <FaYoutube   className='p-2.5 bg-white rounded-full text-btn h-10 w-10 cursor-pointer'/>
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-4 mt-7.5">
+          <div className="grid grid-cols-4 mt-7.5 gap-7.5">
             <div className="gridOne">
               <h3 className='font-bold text-[20px] text-white pb-5'>Information</h3>
               <p className='text-base text-white leading-[27.2px]'>Best Interior Design’s unique perception of the available space renders sophisticated, ergonomic, and luxurious design that transforms a house into a home. The touch has been extended to a variety of platforms, including landscapes, office interiors, villas, communities, retail spaces, and even a single room.</p>
+            </div>
+            <div className="gridTwo">
+              <h3 className='font-bold text-[20px] text-white pb-5'>Services</h3>
+              <ul className='flex flex-col gap-[7.5px]'>
+                <li className='pb-[6.5px] border-b border-white/19 text-base text-white cursor-pointer'>Home Interior Design</li>
+                <li className='pb-[6.5px] border-b border-white/19 text-base text-white cursor-pointer'>Office Interior Design</li>
+                <li className='pb-[6.5px] border-b border-white/19 text-base text-white cursor-pointer'>Showroom Interior Design</li>
+                <li className='pb-[6.5px] border-b border-white/19 text-base text-white cursor-pointer'>Restaurant Interior Design</li>
+                <li className='pb-[6.5px] border-b border-white/19 text-base text-white cursor-pointer'>Exterior Design</li>
+                <li className='text-base text-white cursor-pointer'>Hotel Interior Design</li>
+              </ul>
+            </div>
+            <div className="gridOne">
+              <h3 className='font-bold text-[20px] text-white pb-5'>Contact Us</h3>
+              <div className="flex gap-2.5 items-center">
+                
+              </div>
             </div>
           </div>
         </Container>
